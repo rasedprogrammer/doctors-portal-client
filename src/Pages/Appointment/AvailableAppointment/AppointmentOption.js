ff7020dca67f2a1a3ec8321df201857acc2238c1
rasedprogrammer <rasedprogrammer@gmail.com>
2023-01-07 20:42:49 +0600

    adding appointment option card

--- src/Pages/Appointment/AvailableAppointment/AppointmentOption.js
+++ src/Pages/Appointment/AvailableAppointment/AppointmentOption.js
@@ -1,9 +1,20 @@
 import React from "react";
+import PrimaryButton from "../../../components/PrimaryButton/PrimaryButton";
 
-const AppointmentOption = () => {
+const AppointmentOption = ({ appointmentOption }) => {
+	const { name, slots } = appointmentOption;
 	return (
-		<div>
-			<h1>This is a appointment Option</h1>
+		<div className="card shadow-xl py-5">
+			<div className="card-body items-center text-center">
+				<h2 className="card-title text-secondary">{name}</h2>
+				<p>{slots.length > 0 ? slots[0] : "Try Another Day"}</p>
+				<p>
+					{slots.length} {slots.length > 1 ? "Spaces" : "Space"} Available
+				</p>
+				<div className="card-actions justify-end">
+					<PrimaryButton>Book Appointment</PrimaryButton>
+				</div>
+			</div>
 		</div>
 	);
 };
